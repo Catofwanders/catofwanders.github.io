c4b9b2f6c770aa43c5d50e89fd175ffb4ebdd908
Catofwanders <baturataras@gmail.com>
2020-05-01 11:17:17 +0300

    fix: find pair url

--- showPair/precache-manifest.1a0f1f1793cac5bb5c1bd9819f93936a.js
+++ showPair/precache-manifest.1a0f1f1793cac5bb5c1bd9819f93936a.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6de5e4b0b284953a828f99ba718ae123",
-    "url": "/index.html"
+    revision: "6de5e4b0b284953a828f99ba718ae123",
+    url: "./index.html",
   },
   {
-    "revision": "e34a5f18a7db18727b6c",
-    "url": "/static/css/main.2e7ade5e.chunk.css"
+    revision: "e34a5f18a7db18727b6c",
+    url: "./static/css/main.2e7ade5e.chunk.css",
   },
   {
-    "revision": "3b456122ea71a80f55ee",
-    "url": "/static/js/2.595388d5.chunk.js"
+    revision: "3b456122ea71a80f55ee",
+    url: "./static/js/2.595388d5.chunk.js",
   },
   {
-    "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.595388d5.chunk.js.LICENSE.txt"
+    revision: "c64c486544348f10a6d6c716950bc223",
+    url: "./static/js/2.595388d5.chunk.js.LICENSE.txt",
   },
   {
-    "revision": "e34a5f18a7db18727b6c",
-    "url": "/static/js/main.dcbffe3b.chunk.js"
+    revision: "e34a5f18a7db18727b6c",
+    url: "./static/js/main.dcbffe3b.chunk.js",
   },
   {
-    "revision": "7765d5e03040e88ae5e9",
-    "url": "/static/js/runtime-main.22eb831b.js"
+    revision: "7765d5e03040e88ae5e9",
+    url: "./static/js/runtime-main.22eb831b.js",
   },
   {
-    "revision": "a06220d066a58cda78c2bd37f9e5f3ba",
-    "url": "/static/media/card-back.a06220d0.jpg"
-  }
+    revision: "a06220d066a58cda78c2bd37f9e5f3ba",
+    url: "./static/media/card-back.a06220d0.jpg",
+  },
 ]);
