fe5c26b378d31ab187ddf1494c666afee473e590
Catofwanders <baturataras@gmail.com>
2020-05-03 14:07:50 +0300

    feat: add build tetris

---
+++ tetris/build/precache-manifest.780cf4755d41466e55d4cf73f77bb233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db12743260519d710781a94cc303fab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07e85e21b165ba835ddb",
+    "url": "/static/css/main.66ae509b.chunk.css"
+  },
+  {
+    "revision": "bb7da43d27c9929a72d5",
+    "url": "/static/js/2.ef031153.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ef031153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07e85e21b165ba835ddb",
+    "url": "/static/js/main.2cb9138d.chunk.js"
+  },
+  {
+    "revision": "2d7e2d897c9f48246bd3",
+    "url": "/static/js/runtime-main.0949cfd6.js"
+  }
+]);
